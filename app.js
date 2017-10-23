c34c4fe872785588189be1071f1f9e69aa43ca64
Hector <norzagaray@me.com>
2017-10-23 15:21:31 -0700

    added functions for min and max customers for stores

--- app.js
+++ app.js
@@ -4,33 +4,48 @@
 var firstAndPike = {
   minCust: 23,
   maxCust: 65,
-  avgSale: 6.3
+  avgSale: 6.3,
+  hourlyCustomers: function() {
+    return Math.floor(Math.random() * (firstAndPike.maxCust - firstAndPike.minCust + 1)) + firstAndPike.minCust;
+  },
 };
 
 //second store
 var seatacAirport = {
   minCust: 3,
   maxCust: 24,
-  avgSale: 1.2
+  avgSale: 1.2,
+  hourlyCustomers: function() {
+    return Math.floor(Math.random() * (seatacAirport.maxCust - seatacAirport.minCust + 1)) + seatacAirport.minCust;
+  },
 };
 
 //third store
 var seattleCenter = {
   minCust: 11,
   maxCust: 38,
-  avgSale: 3.7
+  avgSale: 3.7,
+  hourlyCustomers: function() {
+    return Math.floor(Math.random() * (seattleCenter.maxCust - seattleCenter.minCust + 1)) + seattleCenter.minCust;
+  },
 };
 
 //fourth store
 var capitolHill = {
   minCust: 20,
   maxCust: 38,
-  avgSale: 2.3
+  avgSale: 2.3,
+  hourlyCustomers: function() {
+    return Math.floor(Math.random() * (capitolHill.maxCust - capitolHill.minCust + 1)) + capitolHill.minCust;
+  },
 };
 
 //fifth store
 var alki = {
   minCust: 2,
   maxCust: 16,
-  avgSale: 4.6
+  avgSale: 4.6,
+  hourlyCustomers: function() {
+    return Math.floor(Math.random() * (alki.maxCust - alki.minCust + 1)) + alki.minCust;
+  },
 };
