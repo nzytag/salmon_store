6d9ccfb9c36486cd44d4827976de167a8e934410
Hector <norzagaray@me.com>
2017-10-23 14:55:05 -0700

    added some js objects

---
+++ app.js
@@ -0,0 +1,36 @@
+'use strict';
+
+//first store
+var firstAndPike = {
+  minCust: 23,
+  maxCust: 65,
+  avgSale: 6.3
+};
+
+//second store
+var seatacAirport = {
+  minCust: 3,
+  maxCust: 24,
+  avgSale: 1.2
+};
+
+//third store
+var seattleCenter = {
+  minCust: 11,
+  maxCust: 38,
+  avgSale: 3.7
+};
+
+//fourth store
+var capitolHill = {
+  minCust: 20,
+  maxCust: 38,
+  avgSale: 2.3
+};
+
+//fifth store
+var alki = {
+  minCust: 2,
+  maxCust: 16,
+  avgSale: 4.6
+};
