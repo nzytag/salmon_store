4098fa52d6c45f36c185db4c8d69dd0344f5bafe
Hector <norzagaray@me.com>
2017-10-26 20:53:11 -0700

    added new script for greeting depending on time of day

---
+++ script/greetingscript.js
@@ -0,0 +1,18 @@
+'use strict';
+
+
+var today =  new Date ();
+var hourNow = today.getHours();
+var greeting;
+
+if (hourNow > 18) {
+  greeting = 'Good Evening! Welcome to Pat\'s Cookies';
+} else if (hourNow > 12) {
+  greeting = 'Good Afternoon! Welcome to Pat\'s Cookies!';
+} else if (hourNow > 0) {
+  greeting = 'Good Morning!, Welcome to Pat\'s Breakfast place!';
+} else {
+  greeting = 'Welcome!';
+}
+
+document.write('<h1>' + greeting + '<h/1>');
